37efc295989943d5740e3536dcd605adce973560
SofiyaDesaiHPE <sofiya.desai@hpe.com>
2023-05-28 17:24:18 +0530

    commit

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+module "myprovider" {
+  source = "../modules/provider"
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-02eb7a4783e7e9317"
+  instance_type = "t2.micro"
+  tags = {
+    UserName = "Sandeep"
+    Password = "${file("../secretdata.txt")}"
+  }
+}
+
